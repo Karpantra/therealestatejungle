b746e324fd819e47e1b142717165974bb3489e13
Gautier Weigert <gautier.weigert@gmail.com>
2017-10-01 01:23:13 +0200

    chatroom from le wagon

--- config/routes.rb
+++ config/routes.rb
@@ -36,5 +36,9 @@ Rails.application.routes.draw do
 
   resources :briefs
 
+  resources :chat_rooms, only: [:show] do
+    resources :messages, only: [:create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
