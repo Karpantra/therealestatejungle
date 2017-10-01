a6cf4bb738ba817041c57ea0209d440d4ce1a690
Gautier Weigert <gautier.weigert@gmail.com>
2017-10-01 14:25:28 +0200

    infowindow map

--- app/controllers/coworkings_controller.rb
+++ app/controllers/coworkings_controller.rb
@@ -9,17 +9,18 @@ class CoworkingsController < ApplicationController
     #with search from banner
     search = params[:coworking][:address]
       if search.to_s.size > 0
-        @coworkings = policy_scope(Coworking).near(search,5).where.not(latitude: nil, longitude: nil)
+        @coworkings = policy_scope(Coworking).near(search,20).where.not(latitude: nil, longitude: nil)
         @hash = Gmaps4rails.build_markers(@coworkings) do |coworking, marker|
         marker.lat coworking.latitude
         marker.lng coworking.longitude
+        marker.infowindow render_to_string(partial: "/coworkings/map_box", locals: { coworking: coworking })
       end
     else
       @coworkings = policy_scope(Coworking).where.not(latitude: nil, longitude: nil)
       @hash = Gmaps4rails.build_markers(@coworkings) do |coworking, marker|
         marker.lat coworking.latitude
         marker.lng coworking.longitude
-        # marker.infowindow render_to_string(partial: "/coworkings/map_box", locals: { coworking: coworking })
+        marker.infowindow render_to_string(partial: "/coworkings/map_box", locals: { coworking: coworking })
       end
     end
   end
@@ -88,44 +89,3 @@ class CoworkingsController < ApplicationController
       params.require(:coworking).permit(:user_id, :title, :name, :address, :city, :zip_code, :surface, :workstation_number, :price_per_month, :price_per_workstation_per_month, :price_per_workstation_per_day, :price_per_workstation_per_hour, :description, :special_offer, photos: [], category_ids:[], contract_ids:[], type_ids:[], landlord_ids:[])
   end
 end
-#   def coworking_params
-#     params.require(:coworking).permit(:title, :name, :address, :zip_code, :city, :surface, :workstation_number, :description, :special_offer, :user_id, :tv, :erp_room, :auditorium, :kitchen, :nap_zone, :terrace, :internet, :photocopier, :shower, :locker, :garden, :rj45_socket, :optical_fiber, :storage, :furniture, :ohp, :phone, :cooling_system, :alarm, :board, :computer, :waiting_room, :fax, :disabled_access, :bar, :lift, :common_space, :local_bike, :parking, :discretionnary_coffee, :cleaning_service, :welcome_desk_service, :mail_management_service, :office_supplies, :latitude, :longitude, :price_cents, photos: [])
-#   end
-# end
-
-# Controller for simple form with js choice for equipment
-
-# class CoworkingsController < ApplicationController
-#   skip_before_action :authenticate_user!, only: [ :index, :new]
-
-#   def index
-#     @coworkings = Coworking.all
-#   end
-
-#   def new
-#     @coworking = Coworking.new
-#   end
-
-#   def create
-#     @coworking = Coworking.new(coworking_params)
-#     @coworking.user = current_user
-#     if @coworking.save
-#       # coworkingMailer.creation_confirmation(@coworking).deliver_now
-#       flash[:notice] = "Votre espace a bien été créé"
-#       redirect_to coworking_path(@coworking)
-#     else
-#       render new
-#     end
-
-#   end
-
-#   def show
-#     @coworking = Coworking.find(params[:id])
-#   end
-
-#   private
-
-#   def coworking_params
-#     params.require(:coworking).permit(:title, :name, :address, :zip, :city, :surface, :workstation_number, :description, :special_offer, :user_id, :equipment_ids, photos: [])
-#   end
-# end
