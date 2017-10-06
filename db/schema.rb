8b4731b0f8debb4a10916ed63a3c4dcfafb20ffd
Gautier Weigert <gautier.weigert@gmail.com>
2017-10-06 14:16:42 +0200

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005105633) do
+ActiveRecord::Schema.define(version: 20171006082023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 20171005105633) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "owner_id"
+    t.integer "user_id"
+    t.index ["owner_id"], name: "index_contacts_on_owner_id", using: :btree
+    t.index ["user_id"], name: "index_contacts_on_user_id", using: :btree
   end
 
   create_table "contracts", force: :cascade do |t|
