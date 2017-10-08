774e51afa722fa9116836d97e3171713e2eb1e4c
Gautier Weigert <gautier.weigert@gmail.com>
2017-10-09 01:08:30 +0200

    clean config

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  helper :application # gives access to all helpers defined within `application_helper`.
-   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
-   default template_path: 'devise/mailer'
   default from: 'contact@therealestatejungle.fr'
   layout 'mailer'
 end
