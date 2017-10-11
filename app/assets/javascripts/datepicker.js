4cc4babecc0a06b90e479992fbbb858461834465
germainloret <germain.loret@gmail.com>
2017-10-11 11:57:27 +0200

    fix datepicker bug & add style on button in show coworking

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,4 +1,4 @@
-$('#new_demand .input-group.date').datepicker({
+$('#demand_move_in_date').datepicker({
     format: "dd/mm/yyyy",
     weekStart: 1,
     startDate: "today",
@@ -7,25 +7,7 @@ $('#new_demand .input-group.date').datepicker({
     todayHighlight: true
 });
 
-$('#edit_demand .input-group.date').datepicker({
-    format: "dd/mm/yyyy",
-    weekStart: 1,
-    startDate: "today",
-    language: "fr",
-    autoclose: true,
-    todayHighlight: true
-});
-
-$('#new_brief .input-group.date').datepicker({
-    format: "dd/mm/yyyy",
-    weekStart: 1,
-    startDate: "today",
-    language: "fr",
-    autoclose: true,
-    todayHighlight: true
-});
-
-$('#edit_brief .input-group.date').datepicker({
+$('#brief_move_in_date').datepicker({
     format: "dd/mm/yyyy",
     weekStart: 1,
     startDate: "today",
