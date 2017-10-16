eeafd96affc1c30c0586d62d69c36b51f1594a3e
Gautier Weigert <gautier.weigert@gmail.com>
2017-10-16 15:32:25 +0200

    add datepicker to new proposition

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -15,3 +15,21 @@ $('#brief_move_in_date').datepicker({
     autoclose: true,
     todayHighlight: true
 });
+
+$('#proposition_lease_start').datepicker({
+    format: "dd/mm/yyyy",
+    weekStart: 1,
+    startDate: "today",
+    language: "fr",
+    autoclose: true,
+    todayHighlight: true
+});
+
+$('#proposition_mad_start').datepicker({
+    format: "dd/mm/yyyy",
+    weekStart: 1,
+    startDate: "today",
+    language: "fr",
+    autoclose: true,
+    todayHighlight: true
+});
