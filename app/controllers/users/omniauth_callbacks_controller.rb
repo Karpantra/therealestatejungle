0b1d60482afa67ddc1e72d23135a7e81cd88d5af
Gautier Weigert <gautier.weigert@gmail.com>
2017-10-23 16:40:19 +0200

    skip confirmation email with linkedin

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,6 +15,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.find_for_linkedin_oauth(request.env['omniauth.auth'])
 
     if user.persisted?
+      user.skip_confirmation!
       sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Linkedin') if is_navigational_format?
     else
